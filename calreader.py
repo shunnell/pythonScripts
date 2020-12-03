b8f456306cac3ca62ba5de907aa9fe8d4cca705b
Steve Hunnell <stephen.hunnell2@gmail.com>
2020-12-03 12:45:49 -0600

    Add files via upload

---
+++ calreader.py
@@ -0,0 +1,39 @@
+#!/bin/python
+
+import sys
+import subprocess
+
+weekDay = 'monday'
+
+sunday = []
+monday = []
+tuesday = []
+wednesday = []
+thursday = []
+friday = []
+saturday = []
+count = 0
+num = 0
+
+vfile=open('/home/shunnel/development/scripts/cal-data','rt')
+for line in vfile:
+   sunday.append(line[num:num + 2])
+   num += 3
+   monday.append(line[num:num + 2])
+   num += 3
+   tuesday.append(line[num:num + 2])
+   num += 3
+   wednesday.append(line[num:num + 2])
+   num += 3
+   thursday.append(line[num:num + 2])
+   num += 3
+   friday.append(line[num:num + 2])
+   num += 3
+   saturday.append(line[num:num + 2])
+   num = 0
+   if count > 0:
+      if tuesday[count] != "  ":
+         if tuesday[count] != "":
+            print tuesday[count]
+   count += 1
+sys.stdout.close()
---
+++ pue-cfg.py
@@ -0,0 +1,48 @@
+#!/bin/python
+
+import sys
+
+if len(sys.argv) < 2:
+    print "You must enter the PUE number!"
+    sys.exit(128)
+
+pue = str(sys.argv[1])
+pue_ip = "192.168.24."+str((int(pue) + 100))
+pue_hostname = "ifdcgsppupwks"+pue+".boilermaker.gar.us.ray.com"
+
+print "Using hostname: "+pue_hostname
+print "Using ip address: "+pue_ip
+
+part1 = ""
+part2 = ""
+part3 = ""
+
+vfile=open('pue_rhel7.cfg','rt')
+newfile=open("pue_"+pue+".cfg", "w")
+#vfile=open('/var/www/html/ks/pue_rhel7.cfg','rt')
+#newfile=open('/var/www/html/ks/pue_"+pue+".cfg', 'a')
+network_ip = True
+for line in vfile:
+    if line[0:7] == "network" and network_ip == True:
+        columns = line.split(' ')
+        part1 = columns[0] + " " + columns[2] + " " + columns[3]
+            # skipped columns[1] because of double-space
+            # after the first network column
+        part2 = columns[4] + " " + "--ip=" + pue_ip + " " + columns[6]
+        part3 = columns[7] + " " + columns[8] + " " + columns[9]
+        mod_line = part1 + " " + part2 + " " + part3
+        newfile.writelines(mod_line)
+        network_ip = False
+    elif line[0:7] == "network" and network_ip == False:
+        columns = line.split(' ')
+        part1 = columns[0]
+        part2 = "--hostname=" + pue_hostname
+        mod_line = part1 + " " + part2
+        newfile.writelines(mod_line)
+    else:
+        newfile.writelines(line)
+vfile.close()
+newfile.close()
+
+
+
